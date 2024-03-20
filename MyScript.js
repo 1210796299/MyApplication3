651592d66c67cce132bef09e689c7bece2d3c8e6
皮皮熊 <32728352+mxsolid@users.noreply.github.com>
2024-03-20 14:09:04 +0800

    Update MyScript.js

--- MyScript.js
+++ MyScript.js
@@ -5,7 +5,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/user\.chinayanghe\.com\/gateway\/api-integral-mall\/commodity\/getInventoryById? url script-response-body https://raw.githubusercontent.com/mxsolid/MyApplication3/master/MyScript.js
+^https?:\/\/user\.chinayanghe\.com/gateway/api-integral-mall/commodity(getInventoryById|/getById) url script-response-body https://raw.githubusercontent.com/mxsolid/MyApplication3/master/MyScript.js
 
 [mitm]
 hostname = user.chinayanghe.com
@@ -16,6 +16,7 @@ hostname = user.chinayanghe.com
 var body = $response.body;
 
 body = body.replace(/\"resultBody":\d+/g, '\"resultBody":10');
+body = body.replace(/\"inventory":\d+/g, '\"inventory":10');
 console.log(body)
 
 $done({body});
